77fba0279ed8dc3e910efce342ac076a1bf328c6
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2023-03-24 10:03:22 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -38,5 +38,32 @@ let Groupe = 1
 radio.setGroup(Groupe)
 basic.showNumber(Groupe)
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.X) < -255 && input.acceleration(Dimension.Y) < -255) {
+        basic.showArrow(ArrowNames.NorthWest)
+        radio.sendString("AvG")
+    } else if (input.acceleration(Dimension.X) > 255 && input.acceleration(Dimension.Y) < -255) {
+        basic.showArrow(ArrowNames.NorthEast)
+        radio.sendString("AvD")
+    } else if (input.acceleration(Dimension.X) < -255 && input.acceleration(Dimension.Y) > 255) {
+        basic.showArrow(ArrowNames.SouthWest)
+        radio.sendString("ArG")
+    } else if (input.acceleration(Dimension.X) > 255 && input.acceleration(Dimension.Y) > 255) {
+        basic.showArrow(ArrowNames.SouthEast)
+        radio.sendString("ArD")
+    } else if (input.acceleration(Dimension.X) > 255) {
+        basic.showArrow(ArrowNames.East)
+        radio.sendString("D")
+    } else if (input.acceleration(Dimension.X) < -255) {
+        basic.showArrow(ArrowNames.West)
+        radio.sendString("G")
+    } else if (input.acceleration(Dimension.Y) > 255) {
+        basic.showArrow(ArrowNames.South)
+        radio.sendString("Ar")
+    } else if (input.acceleration(Dimension.Y) < -255) {
+        basic.showArrow(ArrowNames.North)
+        radio.sendString("Av")
+    } else {
+        basic.showIcon(IconNames.No)
+        radio.sendString("B")
+    }
 })
